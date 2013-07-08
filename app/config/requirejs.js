9f5fe9afa1c60a3f06ea4c51fd57426e5e5987e3
Ben Alman <cowboy@rj3.net>
2013-07-08 15:08:18 -0400

    RequireJS config visual tweaks.

--- app/config/requirejs.js
+++ app/config/requirejs.js
@@ -5,22 +5,22 @@ require.config({
 
   // Map components to nice paths.
   paths: {
-    text: 'components/requirejs-plugins/lib/text',
-    json: 'components/requirejs-plugins/src/json',
-    hb: 'components/requirejs-handlebars/hb',
+    'text': 'components/requirejs-plugins/lib/text',
+    'json': 'components/requirejs-plugins/src/json',
+    'hb': 'components/requirejs-handlebars/hb',
     'amd-loader': 'components/amd-loader/amd-loader',
-    cjs: 'components/cjs/cjs',
-    handlebars: 'components/handlebars/handlebars',
-    lodash: 'components/lodash/lodash',
-    jquery: 'components/jquery/jquery',
+    'cjs': 'components/cjs/cjs',
+    'handlebars': 'components/handlebars/handlebars',
+    'lodash': 'components/lodash/lodash',
+    'jquery': 'components/jquery/jquery',
     // This must work in the browser AND not explode in the r.js build step.
-    livereload: 'http://' + (typeof location !== 'undefined' ?
+    'livereload': 'http://' + (typeof location !== 'undefined' ?
       location.hostname : 'localhost') + ':35729/livereload.js?snipver=1'
   },
 
   // Load non-AMD dependencies.
   shim: {
-    handlebars: {
+    'handlebars': {
       exports: 'Handlebars'
     }
   },
